3c7cdd7fdc67b09013024e2a5b818b20243f68d1
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-05 19:41:25 -0700

    Initial commit

---
+++ src/Bank.java
@@ -0,0 +1,11 @@
+public class Bank {
+    public static class Banking{
+        public int withdrawl(){
+            if()
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
